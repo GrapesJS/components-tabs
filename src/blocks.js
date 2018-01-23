1b7392640e8df1b4a618e7830ba9489d1eb22146
Artur Arseniev <artur.catch@hotmail.it>
2018-01-23 21:15:37 +0100

    Add tabs SVG icon

--- src/blocks.js
+++ src/blocks.js
@@ -5,8 +5,8 @@ export default (editor, config = {}) => {
 
   tabsBlock && bm.add(type, {
     label: `
-      <svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M22 7.6c0-1-.5-1.6-1.3-1.6H3.4C2.5 6 2 6.7 2 7.6v9.8c0 1 .5 1.6 1.3 1.6h17.4c.8 0 1.3-.6 1.3-1.6V7.6zM21 18H3V7h18v11z" fill-rule="nonzero"/><path d="M4 12.5L6 14v-3zM20 12.5L18 14v-3z"/>
+      <svg class="gjs-block-svg" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+        <path d="M30 0H2a2 2 0 0 0-2 2v22c0 1.1.9 2 2 2h28a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 2v2h-8V2h8zM12 2h8v2h-8V2zm18 22H2V2h8v4h20v18z"/><path d="M5 11c0 .6.4 1 1 1h20a1 1 0 0 0 0-2H6a1 1 0 0 0-1 1zm21 3H6a1 1 0 0 0 0 2h20a1 1 0 0 0 0-2zm0 4H6a1 1 0 0 0 0 2h20a1 1 0 0 0 0-2z"/>
       </svg>
       <div class="gjs-block-label">Tabs</div>
     `,
