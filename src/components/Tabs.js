ab7e265befbfb59e0b5e2d7b29a72f2fba9c5da1
Artur Arseniev <artur.catch@hotmail.it>
2021-02-13 17:11:34 +0100

    Update tabs component to new api

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,62 +1,64 @@
-export default (dc, { defaultModel, defaultView, ...config }) => {
+export default (dc, config) => {
   const type = 'tabs';
   const attrTabs = config.attrTabs;
 
-  dc.addType(type, {
+  const script = function() {
+    var i;
+    var el = this;
+    var attrTabs = '[' + '{[ attr-tabs ]}' + ']';
+    var attrTab = '[' + '{[ attr-tab ]}' + ']';
+    var attrTabContent = '[' + '{[ attr-tab-content ]}' + ']';
+    var classTabActive = '{[ class-tab-active ]}';
+    var selectorTab = '{[ selector-tab ]}';
+    var body = document.body;
+    var matches = body.matchesSelector || body.webkitMatchesSelector
+      || body.mozMatchesSelector || body.msMatchesSelector;
 
-    model: defaultModel.extend({
+    var hideContents = function() {
+      var tabContents = el.querySelectorAll(attrTabContent) || [];
+      for (i = 0; i < tabContents.length; i++) {
+          tabContents[i].style.display = 'none';
+      }
+    }
+
+    var activeTab = function(tabEl) {
+      var tabs = el.querySelectorAll(attrTab) || [];
+
+      for (i = 0; i < tabs.length; i++) {
+          var tab = tabs[i];
+          var newClass = tab.className.replace(classTabActive, '').trim();
+          tab.className = newClass;
+      }
+
+      hideContents();
+      tabEl.className += ' ' + classTabActive;
+      var tabContSelector = tabEl.getAttribute(selectorTab);
+      var tabContent = el.querySelector(tabContSelector);
+      tabContent && (tabContent.style.display = '');
+    };
+
+    var tabToActive = el.querySelector('.' + classTabActive + attrTab);
+    tabToActive = tabToActive || el.querySelector(attrTab);
+    tabToActive && activeTab(tabToActive);
+
+    el.addEventListener('click', function(e) {
+      var target = e.target;
+      matches.call(target, attrTab) && activeTab(target);
+    });
+  };
+
+  dc.addType(type, {
+    isComponent: el => el.hasAttribute && el.hasAttribute(attrTabs),
+
+    model: {
       defaults: {
-        ...defaultModel.prototype.defaults,
         name: 'Tabs',
         'attr-tabs': config.attrTabs,
         'attr-tab': config.attrTab,
         'attr-tab-content': config.attrTabContent,
         'class-tab-active': config.classTabActive,
         'selector-tab': config.selectorTab,
-        script() {
-          var i;
-          var el = this;
-          var attrTabs = '[' + '{[ attr-tabs ]}' + ']';
-          var attrTab = '[' + '{[ attr-tab ]}' + ']';
-          var attrTabContent = '[' + '{[ attr-tab-content ]}' + ']';
-          var classTabActive = '{[ class-tab-active ]}';
-          var selectorTab = '{[ selector-tab ]}';
-          var body = document.body;
-          var matches = body.matchesSelector || body.webkitMatchesSelector
-            || body.mozMatchesSelector || body.msMatchesSelector;
-
-          var hideContents = function() {
-            var tabContents = el.querySelectorAll(attrTabContent) || [];
-            for (i = 0; i < tabContents.length; i++) {
-                tabContents[i].style.display = 'none';
-            }
-          }
-
-          var activeTab = function(tabEl) {
-            var tabs = el.querySelectorAll(attrTab) || [];
-
-            for (i = 0; i < tabs.length; i++) {
-                var tab = tabs[i];
-                var newClass = tab.className.replace(classTabActive, '').trim();
-                tab.className = newClass;
-            }
-
-            hideContents();
-            tabEl.className += ' ' + classTabActive;
-            var tabContSelector = tabEl.getAttribute(selectorTab);
-            var tabContent = el.querySelector(tabContSelector);
-            tabContent && (tabContent.style.display = '');
-          };
-
-          var tabToActive = el.querySelector('.' + classTabActive + attrTab);
-          tabToActive = tabToActive || el.querySelector(attrTab);
-          tabToActive && activeTab(tabToActive);
-
-          el.addEventListener('click', function(e) {
-            var target = e.target;
-            matches.call(target, attrTab) && activeTab(target);
-          });
-        },
+        script,
         ...config.tabsProps
       },
 
@@ -65,15 +67,9 @@ export default (dc, { defaultModel, defaultView, ...config }) => {
         attrs[config.attrTabs] = 1;
         this.setAttributes(attrs);
       }
-    }, {
-      isComponent(el) {
-        if (el.hasAttribute && el.hasAttribute(attrTabs)) {
-          return { type };
-        }
-      },
-    }),
+    },
 
-    view: defaultView.extend({
+    view: {
       init() {
         const comps = this.model.components();
         !comps.length && comps.add(config.template);
@@ -85,6 +81,6 @@ export default (dc, { defaultModel, defaultView, ...config }) => {
           tabContainer.onAdd(tab);
         });
       }
-    })
+    }
   });
 }
